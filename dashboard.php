f50d18c7e0aa5c5f756b45246ae630959e0867ab
sandoval <sandovaltejadadavidsantiago@gmail.com>
2025-11-13 10:30:31 -0500

    mejora de saida

--- dashboard.php
+++ dashboard.php
@@ -1,8 +1,12 @@
 <?php
 session_start();
 if (!isset($_SESSION['usuario'])) {
-    header("Location: index.html");
+    header("Location: index.php");
     exit();
 }
 echo "Hola, " . $_SESSION['nombre'] . ". Has iniciado sesión como " . $_SESSION['rol'];
 ?>
+
+
+<br><br>
+<a href="logout.php">Salir</a>
